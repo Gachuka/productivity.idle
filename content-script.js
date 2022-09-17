168f67ff8e89e8ed28294f11e368d733a27667af
Li Yu <gachuka@gmail.com>
2022-09-17 13:01:16 -0400

    First build mount, minor file tweeks for extension

---
+++ content-script.js
@@ -0,0 +1,26 @@
+console.log('hey')
+const baseUrl = "http://localhost:7878"
+let typrddtring = ''
+
+const downHandler = async (event) => {
+  localStorage.setItem('key', event.key)
+  console.log(event.key)
+  const key = event.key
+  const putBody = JSON.stringify({ key: event.key })
+  const res = await fetch(baseUrl, {        
+    method: "PUT",        
+    mode: "cors",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: putBody 
+  }).then((response) => response.json())
+  .then((result) => {
+    console.log('Success:', result);
+  }).catch((error) => {
+    console.error('Error:', error);
+  });      
+}
+
+window.addEventListener("keydown", downHandler)
