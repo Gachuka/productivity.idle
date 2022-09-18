80a4f2d67d453b7b57df5476d74de9cff5a8a97d
Li Yu <gachuka@gmail.com>
2022-09-17 22:22:42 -0400

    Complete list notLogged

--- content-script.js
+++ content-script.js
@@ -2,30 +2,9 @@ console.log('hey')
 const baseUrl = "http://localhost:7878"
 let typedString = ''
 let characterCount = 0
+let dataLoaded = false
 
-const notLogged = ["Space", "Enter", "Backspace", "Control", "Alt", "Shift", "Tab", "Meta", "ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft", "NumLock", "CapsLock", "Escape"]
-
-const savePeriod = () => {
-  const newTypedString = typedString.slice(-110);
-  localStorage.setItem('typed_string', newTypedString);
-  fetchPUT(newTypedString);
-  console.log('Game Saved');
-  setTimeout(() => {savePeriod()}, 30000);
-}
-
-const downHandler = (event) => {
-
-  if (notLogged.some(string => event.key === string)) {
-    console.log("Not Logged");
-    return;
-  };
-
-  typedString = typedString+event.key;
-  localStorage.setItem('key', event.key);
-  localStorage.setItem('typed_string', typedString.slice(-110));
-  console.log(event.key);
-  const key = event.key;
-}
+const notLogged = ["Space", "Enter", "Backspace", "Control", "Alt", "Shift", "Tab", "Meta", "ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft", "NumLock", "CapsLock", "Escape", "MediaTrackNext", "MediaTrackPrevious", "MediaStop", "MediaPlayPause","AudioVolumeMute", "AudioVolumeDown", "AudioVolumeUp", "LaunchApplication2", "Delete", "Insert", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "PageDown", "PageUp", "Home", "End"]
 
 const fetchData = async (event) => {
   const res = await fetch(baseUrl, {        
@@ -38,6 +17,7 @@ const fetchData = async (event) => {
   localStorage.setItem('character_count', JSON.parse(txt).character_count);
   typedString = JSON.parse(txt).text_typed;
   characterCount = JSON.parse(txt).character_count
+  dataLoaded = true
 }
 
 const fetchPUT = async (typedString) => {
@@ -61,6 +41,29 @@ const fetchPUT = async (typedString) => {
   }); 
 }
 
+const downHandler = (event) => {
+
+  if (notLogged.some(string => event.key === string)) {
+    console.log("Not Logged");
+    return;
+  };
+
+  typedString = typedString+event.key;
+  localStorage.setItem('key', event.key);
+  localStorage.setItem('typed_string', typedString.slice(-110));
+  console.log(event.key);
+  const key = event.key;
+}
+
+const savePeriod = () => {
+  const newTypedString = typedString.slice(-110);
+  localStorage.setItem('typed_string', newTypedString);
+  fetchPUT(newTypedString);
+  console.log('Game Saved');
+  setTimeout(() => {savePeriod()}, 30000);
+}
+
 window.addEventListener("keydown", downHandler);
 fetchData();
-savePeriod();
+
+setTimeout(savePeriod, 30000);
